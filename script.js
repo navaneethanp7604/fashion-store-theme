c4f227c0ce5d7d158c80aaf7914467149f7a99a3
navaneethanp7604 <navaneethanp7604@gmail.com>
2024-12-06 12:33:53 +0530

    created fashion

---
+++ script.js
@@ -0,0 +1,9 @@
+//navbar-section
+
+var sidenav=document.querySelector(".side-navbar")
+function shownavbar(){
+    sidenav.style.left="0"
+}
+function closenavbar(){
+    sidenav.style.left="-60%"
+}
